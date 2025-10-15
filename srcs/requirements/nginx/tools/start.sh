eacf073e4bbfa645c6eecfe92d5273a5cd822d87
lynn <lynnyoussef36@gmail.com>
2025-10-15 10:54:00 +0300

    last push

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-
-# Exit on error
 set -e
 
-# Generate SSL certificates
-/generate_ssl.sh
+# Generate SSL certificates if they don't exist
+if [ ! -f "/etc/nginx/ssl/nginx.key" ] || [ ! -f "/etc/nginx/ssl/nginx.crt" ]; then
+    echo "Generating SSL certificates for ${DOMAIN_NAME}..."
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/nginx.key \
+        -out /etc/nginx/ssl/nginx.crt \
+        -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=42/CN=${DOMAIN_NAME}"
+    chmod 600 /etc/nginx/ssl/nginx.key
+    chmod 644 /etc/nginx/ssl/nginx.crt
+fi
 
 # Replace environment variables in configuration templates
 envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/default.conf.template > /etc/nginx/http.d/default.conf
-envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/http-redirect.conf.template > /etc/nginx/http.d/http-redirect.conf
 
-# Test NGINX configuration
+# Test and start NGINX
 nginx -t
-
-# Start NGINX
 exec /usr/sbin/nginx -g "daemon off;"
